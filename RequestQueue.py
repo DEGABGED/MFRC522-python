8a470b568b03217eff5349a1c5042e8a18fba1a4
Francis dela Cruz <franciszac.dlc@gmail.com>
2018-11-11 21:35:01 +0800

    Add sending of post requests

--- RequestQueue.py
+++ RequestQueue.py
@@ -1,10 +1,17 @@
 from Queue import Queue
+import threading
+import json
+
+import requests
 
 class RequestQueue:
     Size = 300
 
     def __init__(self):
         self.queue = Queue(self.Size)
+        thread = threading.Thread(target=self.run, args=())
+        thread.daemon = True
+        thread.start()
 
     def get_queue(self):
         return self.queue
@@ -13,3 +20,24 @@ class RequestQueue:
         while not self.queue.empty():
             obj = self.queue.get()
             print obj
+
+    def send(self, payload):
+        # Send the json
+        try
+            res = requests.post(self.url, payload)
+            return true
+        except requests.ConnectionError
+            return false
+
+    def run(self):
+        while(True):
+            # Check the queue for jobs
+            if self.queue.empty():
+                continue
+
+            job = json.dumps(self.queue.get())
+            while(not self.send(job))
+
+            # Finished
+            self.queue.task_done()
+
